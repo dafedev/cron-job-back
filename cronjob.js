65d9787b27a783f0a408d648f8c1a5f52bbe69ac
YeisonReactDev <react1.simbiosistemas@gmail.com>
2023-05-30 14:52:50 -0500

    Ejecucion de funcion por script (sin cron)

--- cronjob.js
+++ cronjob.js
@@ -2,9 +2,7 @@ import cron from "node-cron";
 import axios from "axios";
 import MailService from "./services/MailService.js";
 
-// 0 19 * * * => 7:00 pm
-
-cron.schedule("* * * * *", async () => {
+const main = async (exitProcess = false) => {
   try {
     const response = await axios.get(process.env.API_BACK, {
       headers: {
@@ -15,11 +13,19 @@ cron.schedule("* * * * *", async () => {
     const { emails, message, subject } = response.data;
 
     if (!emails && !subject) {
-      return console.log("No hay correos para enviar.");
+      console.log("No hay correos para enviar.");
+      return exitProcess && process.exit(1)
     } else {
-      return MailService.sendMail(emails, subject, message);
+      await MailService.sendMail(emails, subject, message);
+      return exitProcess && process.exit(1)
     }
   } catch (error) {
-    return console.log(error);
+    console.log(error);
+    return exitProcess && process.exit(1)
   }
-});
+}
+
+// 0 19 * * * => 7:00 pm
+// cron.schedule("* * * * *", () => main());
+
+main(true)
